349e33a9df3f8b8f13229b8c42ba404b5d748757
Joy Ge <joy.ge@oocl.com>
2016-06-01 17:05:56 +0800

    add first slide and sample test

---
+++ reveal.js/test/js/test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+describe('UpGoing', function() {
+    describe('statement return', function() {
+        it('should return null', function() {
+            var a;
+            assert.equal("undefined", typeof a);
+        });
+    });
+});
